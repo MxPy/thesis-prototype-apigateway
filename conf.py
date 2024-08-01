b57508e9424766461cf781d67ffcbd8ec34c75e1
Maximilian Patalas <maksymilianpatalas@gmail.com>
2024-08-01 19:28:26 +0200

    change conf for compose

--- conf.py
+++ conf.py
@@ -6,7 +6,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     #TODO: move to env
     #change users to localhost for nondocker run
-    USERS_SERVICE_URL: str = "http://localhost:8000"
+    USERS_SERVICE_URL: str = "http://users:8000"
     GATEWAY_TIMEOUT: int = 59
     ACCESS_TOKEN_DEFAULT_EXPIRE_MINUTES: int = 360
 
