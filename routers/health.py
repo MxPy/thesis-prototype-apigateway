3344fbaadd2ef4a53349988779731fe9711df2c1
Maximilian Patalas <maksymilianpatalas@gmail.com>
2025-01-14 21:25:31 +0100

    paging

--- routers/health.py
+++ routers/health.py
@@ -27,7 +27,7 @@ async def get_all(data: User, request: Request, response: Response, session_id: 
 
 @route(
     request_method=router.get,
-    path='/users/',
+    path='/users',
     status_code=status.HTTP_200_OK,
     payload_key='',
     service_url=settings.HEALTH_SERVICE_URL,
@@ -48,20 +48,6 @@ async def get_all(userId: str, request: Request, response: Response, session_id:
     privileges_level=0,
 )
 async def get_all(userId: str, data: UpdateUserRequest, request: Request, response: Response,
-                  session_id: str = Header(...)):
-    pass
-
-
-@route(
-    request_method=router.get,
-    path='/users',
-    status_code=status.HTTP_200_OK,
-    payload_key='data',
-    service_url=settings.HEALTH_SERVICE_URL,
-    authentication_required=True,
-    privileges_level=1,
-)
-async def get_all(userId: str, request: Request, response: Response,
                   session_id: str = Header(...)):
     pass
 
